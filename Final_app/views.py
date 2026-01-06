0de5fee95e7a387124e5a5e5750b25bbb3d1477f
Farha2318 <farhanabegam501@gmail.com>
2026-01-06 12:59:23 +0530

    Initial commit without venv

---
+++ Final_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
+from .services import create_student
+from .services import get_students, create_student
+
+async def student_list(request):
+    students = await get_students()
+    data = [{"id": s.id, "name": s.name} for s in students]
+    return JsonResponse(data, safe=False)
+
+async def add_student(request):
+    name = request.GET.get("name")
+    email = request.GET.get("email")
+
+    student = await create_student(name, email)
+    return JsonResponse({"id": student.id, "name": student.name})
+
+@csrf_exempt
+async def add_student_post(request):
+    if request.method == "POST":
+        body = json.loads(request.body)
+        student = await create_student(
+            body["name"],
+            body["email"]
+        )
+        return JsonResponse({
+            "id": student.id,
+            "name": student.name
+        })
+
